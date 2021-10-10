d95e78b7b3f995d83e8a69bc2de784dd5092a38c
greg <greg@landing.ai>
2021-10-10 08:05:11 -0700

    first commit

---
+++ map/start-dev.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Safely execute this bash script
+# e exit on first failure
+# u unset variables are errors
+# f disable globbing on *
+# pipefail | produces a failure code if any stage fails
+set -euf -o pipefail
+
+# Get the directory of this script
+LOCAL_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
+#start the web server
+python3 -m http.server
